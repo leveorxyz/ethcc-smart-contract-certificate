e3f22b6e502c4b0bb80da10f02cdfd9c14cad667
sihan-tawsik <sihantawsik@gmail.com>
2022-07-22 17:10:43 +0600

    Add verifier and revoke functionality

--- contracts/SBT.sol
+++ contracts/SBT.sol
@@ -9,6 +9,16 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract SBT is ERC721, Ownable, EIP712, ERC721Votes {
     using Counters for Counters.Counter;
+    mapping (uint => status) tokenStatus;
+    mapping (address => bool) isVerifier;
+    mapping (uint => address) verifier;
+    uint256[] private revokedList;
+
+    enum status {
+        NOT_VERIFIED,
+        VERIFIED,
+        REVOKED
+    }
 
     Counters.Counter private _tokenIdCounter;
 
@@ -22,6 +32,7 @@ contract SBT is ERC721, Ownable, EIP712, ERC721Votes {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
+        tokenStatus[tokenId] = status.NOT_VERIFIED;
     }
 
     // The following functions are overrides required by Solidity.
@@ -38,6 +49,44 @@ contract SBT is ERC721, Ownable, EIP712, ERC721Votes {
     {
         require(from == address(0), "Err: token is SOUL BOUND");
         super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    modifier notRevoked (uint256 tokenId) {
+        require(tokenStatus[tokenId] < status.REVOKED, "The token is already revoked");
+        _;
+    }
+
+    modifier onlyExistingToken (uint tokenId) {
+        require (tokenId <= _tokenIdCounter.current(), "Invalid token id");
+        _;
+    }
+
+    modifier onlyVerifier (uint tokenId) {
+        require (verifier[tokenId] == msg.sender || msg.sender == owner(), "Only the verifier can revoke");
+        _;
+    }
+
+    modifier ifVerifier {
+        require(isVerifier[msg.sender] || msg.sender == owner(), "Only verifiers allowed");
+        _;
+    }
+
+    function makeVerifier(address _address) external onlyOwner {
+        isVerifier[_address] = true;
+    }
+
+    function verify (uint tokenId) external ifVerifier onlyExistingToken(tokenId) notRevoked(tokenId) {
+        tokenStatus[tokenId] = status.VERIFIED;
+        verifier[tokenId] = msg.sender;
+    }
+
+    function revoke(uint256 tokenId) external onlyExistingToken(tokenId) notRevoked(tokenId) onlyVerifier(tokenId) {
+        tokenStatus[tokenId] = status.REVOKED;
+        revokedList.push(tokenId);
+    }
+
+    function getRevokedTokens() external view returns(uint256[] memory) {
+        return revokedList;
     }
 
 }
